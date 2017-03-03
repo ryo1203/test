94b9879b1ac92f0db9e9535fb3fb8eeafdb8c32e
ryo1203 <1203ryo1999@gmail.com>
2017-03-03 20:48:40 +0900

    update

--- test2/script.js
+++ test2/script.js
@@ -1,5 +1,15 @@
 $(function(){
 
+/*$('button').click(function(){
+  var p =  $('<p>').text('vanish').addClass('vanish');
+  $(this).after(p);
+});
+
+$('body').on('click','.vanish',function(){
+  $(this).remove();
+});*/
+
+
 
 
 });
